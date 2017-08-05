f5c3fec179cc2e710f63d55878caace1128f2aab
Jacob Polloreno <makaveliix16@gmail.com>
2017-08-05 08:26:46 -0700

    update part 3 testing

--- src/test/test_part_3.py
+++ src/test/test_part_3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 """
-Tests against My History Skill webapp logic
+Tests Part 3 against My History Skill webapp logic
 """
 
 import json
@@ -11,32 +11,41 @@ from test.conftest import post
 from test.utils import _read_facts
 
 
-messages = _read_facts()
-fact_msgs = {k: v for k, v in messages.items() if 'get_fact_message'}
-facts = {k: v for k, v in messages.items() if 'facts_en_' in k}
+MESSAGES = _read_facts()
+FACT_MSGS = {k: v for k, v in MESSAGES.items() if 'get_fact_message_' in k}
+FACTS = {k: v for k, v in MESSAGES.items() if 'facts_en_' in k}
+RESULTS = []
 
-resultArr = []
 
+def test_conv_getnewfactintent_1(client):
+    """
+    User should get a new fact and be reprompted
 
-def test_conversational_ele_GetNewFactIntent_1(client):
-    response = post(client, requests.get_new_year_fact())
+    :param client
+    """
+    response = post(client, requests.get_new_fact())
 
     assert response['response']['outputSpeech']['text'],\
         "should have a response"
 
-    resultArr.append(response['response']['outputSpeech']['text'])
+    RESULTS.append(response['response']['outputSpeech']['text'])
 
     assert response['response']['reprompt'],\
         "should have reprompt"
 
 
-def test_conversational_GetNewFactIntent_2(client):
-    response = post(client, requests.get_new_year_fact())
+def test_conv_getnewfactintent_2(client):
+    """
+    User should get a new fact and Amazon should keep the session open
+
+    :param client
+    """
+    response = post(client, requests.get_new_fact())
 
     assert response['response']['outputSpeech']['text'],\
         "should have a response"
 
-    resultArr.append(response['response']['outputSpeech']['text'])
+    RESULTS.append(response['response']['outputSpeech']['text'])
 
     assert response['response']['shouldEndSession'] is not None,\
         "should not end the alexa session"
@@ -44,29 +53,54 @@ def test_conversational_GetNewFactIntent_2(client):
         "should not end the alexa session"
 
 
-def test_conversational_GetNewFactIntent_3(client):
-    response = post(client, requests.get_new_year_fact())
+def test_conv_getnewfactintent_3(client):
+    """
+    User should get a new fact, Amazon should keep the session open,
+    and should include at least 5 different phrase options to prefix
+    the get fact message. Further, the prefix should be randomly
+    assigned.
+
+    :param client
+    """
+    response = post(client, requests.get_new_fact())
 
     assert response['response']['outputSpeech']['text'],\
         "should have a response"
 
-    resultArr.append(response['response']['outputSpeech']['text'])
+    RESULTS.append(response['response']['outputSpeech']['text'])
 
     assert response['response']['shouldEndSession'] is not None,\
         "should not end the alexa session"
     assert not response['response']['shouldEndSession'],\
         "should not end the alexa session"
 
-    assert len(fact_msgs) >= 5,\
+    assert len(FACT_MSGS) >= 5,\
         "should include at least 5 different phrase options"
 
-    assert 
+    included_fact_msgs = []
 
-def test_conversational_GetNewFactIntent_valid_year(client):
-    facts_str = ' '.join([v for k, v in facts.items()])
-    years = re.findall('\d{4}', facts_str)
+    for r in RESULTS:
+        for f_k, f_v in FACT_MSGS.items():
+            if f_v in r:
+                included_fact_msgs.append(f_k)
 
-    if len(years) > 0:
+    assert len(set(included_fact_msgs)) > 1,\
+        "should randomly include segments from the GET_FACT_MESSAGE templates"
+
+
+def test_conv_getnewyearfactintent_valid_year(client):
+    """
+    User should get a new fact based on a valid year, Amazon
+    should keep the session open, and should reprompt user.
+
+    :param client
+    """
+
+    # Get some valid years
+    facts_str = ' '.join([v for k, v in FACTS.items()])
+    years = re.findall(r'\d{4}', facts_str)
+
+    if not years:
         # Grab invalid year(9999) event
         event = json.load(requests.get_new_year_fact())
         # Modify the year value in the json dict
@@ -83,7 +117,13 @@ def test_conversational_GetNewFactIntent_valid_year(client):
         "should not end the alexa session"
 
 
-def test_conversational_GetNewFactIntent_non_matching(client):
+def test_conv_getnewyearfactintent_non_matching(client):
+    """
+    User should get a new fact based on a invalid year, Amazon
+    should keep the session open, and should reprompt user.
+
+    :param client
+    """
     response = post(client, requests.get_new_year_fact())
 
     assert response['response']['reprompt'],\
