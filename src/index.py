c9528edb5a5ae68b69b32673799bfc37c1e945da
Jacob Polloreno <jacob.polloreno@gmail.com>
2017-08-06 16:40:00 -0700

    add part 3 solutions

--- src/index.py
+++ src/index.py
@@ -22,12 +22,12 @@ TEMPLATES_PATH = os.path.join(os.path.dirname(__file__), 'templates.yaml')
 # (Part 2) provide a function for the new intent named 'GetYearFact'
 #     that emits a randomized fact that includes the year requested by the user
 #     - if such a fact is not available, tell the user this and provide an alternative fact.
-# TODO (Part 3) Keep the session open by providing the fact with 'question'
+# (Part 3) Keep the session open by providing the fact with 'question'
 # instead of 'statement'. Be sure to include a card (e.g 'simple_card') with response.
 #     - make sure the user knows that they need to respond
 #     - provide a reprompt that lets the user know how they can respond
-# TODO (Part 3) Provide a randomized response for the GET_FACT_MESSAGE
-#     - add new messages to the templates.yaml file beggining with
+# (Part 3) Provide a randomized response for the GET_FACT_MESSAGE
+#     - add new messages to the templates.yaml file prefixed with
 #     'get_facts_message_'
 #     - randomize this starting portion of the response for conversational
 #     variety(already implemented for you, see get_fact() line 49)
@@ -59,12 +59,16 @@ def get_fact():
     factMsgs = [msg for msg in templates if 'get_fact_message' in msg]
     randomMsg = render_template(random.choice(factMsgs))
 
+    # Get repeat output
+    repeat_msg = render_template('reprompt_msg')
+
     # Create speech output
-    speechOutput = randomMsg + ' : ' + randomFact
+    speechOutput = randomMsg + ' : ' + randomFact + ' ... ' + repeat_msg
 
     card_title = render_template('skill_name')
-    return statement(speechOutput).simple_card(title=card_title,
-                                               content=randomFact)
+    reprompt_msg = render_template('help_message')
+    return question(speechOutput).simple_card(
+        title=card_title, content=randomFact).reprompt(reprompt_msg)
 
 
 @ask.intent("GetNewYearFactIntent")
@@ -89,12 +93,16 @@ def get_new_year_fact(FACT_YEAR):
     get_fact_msgs = [msg for msg in templates if 'get_fact_message' in msg]
     random_msg = render_template(random.choice(get_fact_msgs))
 
+    # Get repeat output
+    repeat_msg = render_template('reprompt_msg')
+
     # Create speech output
-    speech_output = random_msg + ' : ' + fact_msg
+    speech_output = random_msg + ' : ' + fact_msg + ' ... ' + repeat_msg
 
     card_title = render_template('skill_name')
-    return statement(speech_output).simple_card(title=card_title,
-                                                content=fact_msg)
+    reprompt_msg = render_template('help_message')
+    return question(speech_output).simple_card(
+        title=card_title, content=fact_msg).reprompt(reprompt_msg)
 
 
 @ask.intent("AMAZON.HelpIntent")
